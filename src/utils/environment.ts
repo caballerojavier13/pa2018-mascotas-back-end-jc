1464689cde55b25dd4aa2224be8f06821e78dbc4
Javier Caballero <Javier.X.Caballero.-ND@disney.com>
2018-04-18 19:02:03 -0300

    Editando modo de conexión a Redis

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -34,7 +34,6 @@ export interface Config {
   logLevel: string; // 'debug' | 'verbose' | 'info' | 'warn' | 'error';
   mongoDb: string;
   redisHost: string;
-  redisPort: number;
   passwordSalt: string;
   jwtSecret: string;
 }
